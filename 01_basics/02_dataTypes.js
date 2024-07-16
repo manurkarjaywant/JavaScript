8a1ccbd7f3fd09c5608bba29f1b962397a9a52ed
manurkarjaywant <147039486+manurkarjaywant@users.noreply.github.com>
2024-07-16 15:46:48 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello"); // we are using nodejs, not browser
+
+let name = "jaywant"
+let age = 32
+let isLoggedIn = false
+let state;
+
+// number
+// bigint
+// string
+// boolean
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);   // object
