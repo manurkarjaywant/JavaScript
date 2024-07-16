f0e3054d7ea3fedf0faa531c721cacd9ee1d5de2
manurkarjaywant <147039486+manurkarjaywant@users.noreply.github.com>
2024-07-16 15:47:17 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "jaywant@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "Bangaluru"
+
+//console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
