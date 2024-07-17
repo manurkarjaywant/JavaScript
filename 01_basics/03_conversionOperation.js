7480604cc9bbc93772e4c738e6869bcf4ba7a3a7
manurkarjaywant <147039486+manurkarjaywant@users.noreply.github.com>
2024-07-17 07:49:58 +0000

    conversion and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,4 @@
+/*
 let score = "33abc"
 
 console.log(typeof score);
@@ -6,22 +7,70 @@ console.log(typeof(score));
 let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+*/
 
 //"33" => 33
 // "33abc" => NaN
 // true => 1; false => 0;
 
+/*
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 console.log(booleanIsLoggedIn);
+*/
 
 // 1 => true; 0 => false
 // "" => false
 // "jaywant" => true
 
+/*
 let someNumber = 33
 
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+
+// **************************    Operations  ***********************************
+
+let value = 3;
+let negValue = -value;
+
+/*
+console.log(negValue);
+*/
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**3);
+*/
+
+/*
+console.log("1" + 2);   // 12
+console.log(1 + "2");   // 12
+console.log("1" + 2 + 2);   // 122
+console.log(1 + 2 + "2");   // 32
+*/
+
+/*
+console.log(true);
+console.log(+true);
+console.log(+"");
+*/
+
+/*let num1 = num2 = num3 = 2+2;
+console.log(num1);
+console.log(num2);
+console.log(num3);
+*/
+
+/*
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+*/
 
