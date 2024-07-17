02215b558c82ae32b1e32dff7547876d98a06476
manurkarjaywant <147039486+manurkarjaywant@users.noreply.github.com>
2024-07-17 08:18:41 +0000

    comparisons

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,8 +2,17 @@ const accountId = 144553
 let accountEmail = "jaywant@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
+const accountId = 144553
+let accountEmail = "jaywant@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
 let accountState;
 
+<<<<<<< HEAD
+accountId = 2
+
+console.log(accountId);
+=======
 // accountId = 2 // not allowed
 
 accountEmail = "hc@hc.com"
@@ -13,3 +22,4 @@ accountCity = "Bangaluru"
 //console.log(accountId);
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+>>>>>>> b1b439f56abc7a8daa732f61b4dad81d0d1272dd
---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,34 @@
+/*
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+*/
+
+/*
+console.log("2" > 1);
+console.log("02" > 1);
+console.log("2" < 1);
+console.log("02" < 1);
+*/
+
+/*
+console.log(null > 0);          //  when comparison is used in an expression ( <, >, <=, >=) null is converted into 0.
+console.log(null == 0);         //  when equality is used in an expression ( == ) null is considered as null only. 
+console.log(null >= 0);
+*/
+
+/*
+console.log(undefined == 0);    // false
+console.log(undefined > 0);     // false
+console.log(undefined < 0);     // false
+*/
+
+// ******************** Tripple Equal to ( === ) **************************
+/*
+console.log("2" == 2);      // it will check only value
+console.log("2" == 3);      // it will check only value
+console.log("2" === 2);     // it will check value as well as datatype
+console.log("2" === 3);     // it will check value as well as datatype
+*/
