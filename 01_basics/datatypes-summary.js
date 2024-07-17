58ed4cb0aa926f8b836672dd44d62de78b65f0ce
manurkarjaywant <147039486+manurkarjaywant@users.noreply.github.com>
2024-07-17 08:55:09 +0000

    datatypes summery

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+/*const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+*/
+
+/*
+const bigNumber = 54435433548546631546543n  // put n after the number and it will be converted into BigInt
+console.log(typeof bigNumber);
+*/
+
+// ************************* Reference type / Non-Primitive **************************
+
+// ************************** Array, Objects, Functions ******************************
+
+/*const heros = ["shaktimaan", "naagraj", "doga"]
+
+let myObject = {
+    name: "jaywant",
+    age: 32,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof heros);
+console.log(typeof myObject);
+console.log(typeof myFunction);
+*/
+
+// typeof non premitive datatypes will always be "object" and typeof function will be "function" but it is called as "object function".
