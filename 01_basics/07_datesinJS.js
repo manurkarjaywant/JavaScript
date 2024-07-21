7b72aa690b49694ac56c90549336801efc68935f
manurkarjaywant <147039486+manurkarjaywant@users.noreply.github.com>
2024-07-21 11:15:42 +0000

    dates in javascript

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,29 @@
+// Dates
+
+let myDate = new Date()
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedDate = new Date(2024, 1, 23) // in Javascript months start from 0 i.e. 0= january, 1= february and so on.
+let myCreatedDate = new Date(2024, 6, 21, 15, 20)
+// let myCreatedDate = new Date("2024-12-31") // yyyy-mm-dd valid // yyyy-dd-mm not valid
+// let myCreatedDate = new Date("12-31-2024") // mm-dd-yyyy valid // dd-mm-yyyy not valid
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(myCreatedDate.getTime()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()); //  january is 0
+console.log(newDate.getDay()); // sunday is 0
+
