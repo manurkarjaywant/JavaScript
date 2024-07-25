4b8f1c69ed18038e8d5e5ae2a3f595ec68eefada
manurkarjaywant <147039486+manurkarjaywant@users.noreply.github.com>
2024-07-25 09:46:41 +0000

    truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,40 @@
+// const userEmail = "h@jaywant.ai" // try with empty strings ""," ", []
+
+// if (userEmail) {
+//     console.log("got user email");
+// } else {
+//     console.log("Don't have user email");
+// }
+
+// falsy values
+// false, 0, -0, bigInt 0n, "", null, undefined, NaN
+
+// truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// const userEmail = []
+// if (userEmail.length === 0) {
+//     console.log("array is empty");
+// }
+
+// const emptyObject = {}
+// if (Object.keys(emptyObject).length === 0) {
+//     console.log("object is empty");
+// }
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 15
+// val1 = undefined ?? 20
+// val1 = null ?? 10 ?? 20
+
+// console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
