52831d4b96cbe920a28b1378e7bc1e58639d2f83
manurkarjaywant <147039486+manurkarjaywant@users.noreply.github.com>
2024-07-28 13:37:23 +0000

    work with functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,3 +1,4 @@
+/*
 function sayMyName () {
     console.log("J");
     console.log("A");
@@ -7,26 +8,28 @@ function sayMyName () {
     console.log("N");
     console.log("T");
 }
+sayMyName();
+*/
 
-// sayMyName();
-
-// function addTwoNumbers (number1, number2){
-
-//     console.log(number1 + number2);
-// }
-
+/*
 function addTwoNumbers (number1, number2){
-
-    // let result = number1 + number2
-    // return result
-    return number1 + number2
+    console.log(number1 + number2);
 }
-
 const result = addTwoNumbers(3,4);
+console.log(`Result : ${result}`);
+*/
 
-// console.log(`Result : ${result}`);
+/*
+function addTwoNumbers (number1, number2){
+    let result = number1 + number2
+    return result
+    // return number1 + number2
+}
+const result = addTwoNumbers(3,4);
+console.log(`Result : ${result}`);
+*/
 
-
+/*
 function loginUserMessage(username){
     if(!username){
         console.log("please enter a username");
@@ -34,42 +37,47 @@ function loginUserMessage(username){
     }
         return `${username} just logged in`
 }
-
-// console.log(loginUserMessage("jaywant"))
+console.log(loginUserMessage("jaywant"))
+console.log(loginUserMessage(""))
+*/
 
 /*
  "..." is know as "rest operator" as well as "spread operator" depending on its usage
 */
 
-// function calculateCartPrice(...num1){ // output =>[ 200, 400, 500, 2000 ] => here num1 will take all the arguments passed
-//     return num1
-// }
+/*
+function calculateCartPrice(...num1){ // output =>[ 200, 400, 500, 2000 ] => here num1 will take all the arguments passed
+    return num1
+}
+console.log(calculateCartPrice(200,400,500,2000));
+*/
 
-// function calculateCartPrice(val1, val2, ...num1){ // output =>[ 500, 2000 ] => here val1 and val2 will take 200 and 400. rest everything will be taken by num1.
-//     return num1
-// }
-
-// console.log(calculateCartPrice(200,400,500,2000));
+/*
+function calculateCartPrice(val1, val2, ...num1){ // output =>[ 500, 2000 ] => here val1 and val2 will take 200 and 400. rest everything will be taken by num1.
+    return num1
+}
+console.log(calculateCartPrice(200,400,500,2000));
+*/
 // ----------------------------------------------------------------------------
 
 // ****************** Passing object into the function **************************
-const user = {
-    username: "jaywant",
-    price: 199
-}
-
+/*
+// const user = {
+//     username: "jaywant",
+//     price: 199
+// }
 function handleObject(anyobject){
-    // console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
 }
-
 // handleObject(user) // we can pass the object into function like this
 handleObject({         // we can pass the object into function like this also
     username: "jaywant",
     price: 399
 })
+*/
 
 // ********************** passing an array into the function ***************************
-
+/*
 const myNewArray = [100, 200, 300, 400]
 
 function returnSecondValue (getArray){
@@ -77,3 +85,4 @@ function returnSecondValue (getArray){
 }
 // console.log(returnSecondValue(myNewArray));// we can pass an array into function like this
 console.log(returnSecondValue([100, 200, 300, 400]));// we can pass an array into function like this also
+*/
