6ac7241cb26a79803ee3f4b91b67bd0f715e7da1
manurkarjaywant <147039486+manurkarjaywant@users.noreply.github.com>
2024-07-28 06:03:43 +0000

    string methods

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,26 +1,25 @@
+/*
 const name = "jaywant"
 const repoCount = 50
+console.log(name + repoCount + " value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  // this is string interpolation
+*/
 
-// console.log(name + repoCount + " value");
+/*
+const gameName = new String('jaywant-jm')
+// console.log(gameName[0]);   // output => j
+// console.log(gameName.__proto__);   // output => {}
+// console.log(gameName.length);   // output => 10
+// console.log(gameName.toUpperCase());   // output => JAYWANT-JM
+// console.log(gameName.charAt(5));   // output => n
+// console.log(gameName.indexOf('n'));   // output => 5
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// const newString = gameName.substring(0,4)
+// console.log(newString);   // output => jayw 
 
-const gameName = new String('jaywant-hc')
+const anotherString = gameName.slice(-8,4)  //  j:1  a:2  y:3  w:4  a:5  n:6  t:7  -:8  j:9  m:10 ========  m:-1  j:-2  -:-3  t:-4  n:-5  a:-6  w:-7  y:-8  a:-9  j:-10
+console.log(anotherString);   // output => yw  => it will take all the string from -8 to 4 including -8 and 4
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
-
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(5));
-console.log(gameName.indexOf('n'));
-
-const newString = gameName.substring(0,4)
-console.log(newString);
-
-const anotherString = gameName.slice(-8,4)
-
-console.log(anotherString);
-
-const newStringOne = "    jaywant Manurkar    "
-console.log(newStringOne.trim());
+// const newStringOne = "    jaywant Manurkar    "
+// console.log(newStringOne.trim());   // output => jaywant Manurkar
+*/
