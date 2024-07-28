3ed169d5bb409368518740bedf64b5bed9301001
manurkarjaywant <147039486+manurkarjaywant@users.noreply.github.com>
2024-07-28 13:45:01 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,18 +1,20 @@
+/*
 if(true){
-    let a = 10
-    const b = 20
+    const a = 10
+    let b = 20
     var c = 30
 }
-
-// console.log(a); // not available outside the scope
-// console.log(b); // not available outside the scope
-// console.log(c); // available outside the scope
+console.log(a); // not available outside the scope
+console.log(b); // not available outside the scope
+console.log(c); // available outside the scope
+*/
 
 /*
 global scope when executed through browser is different and 
 global scope when executed through node is different.
 */
 
+/*
 function one() {
     const username = "jaywant"
 
@@ -24,9 +26,10 @@ function one() {
     two() 
  
 }
-
 // one()
+*/
 
+/*
 if (true) {
     const username = "jaywant"
     if (username === "jaywant") {
@@ -36,22 +39,26 @@ if (true) {
     // console.log(website);
 }
 // console.log(username)
+*/
 
 // ***************** interesting ***********************
-
+/*
 addOne(5)
 function addOne(num) {
     return num + 1
 }
+*/
 /*
 in above example addOne is a function name and it is called before it is defined.
 no errors were occured
 */
 
+/*
 addTwo(5)
 const addTwo = function (num) {
     return num + 2    
 }
+    */
 /*
 in above example addTwo is a variable name in which a functione is stored.
 error was occured as variable is used before it is initialized.
