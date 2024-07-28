e8deed56555181aa3599f4ddd4a77446ffec5d11
manurkarjaywant <147039486+manurkarjaywant@users.noreply.github.com>
2024-07-28 05:43:34 +0000

    comparisons

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -1,16 +1,16 @@
 /*
-console.log(2 > 1);
-console.log(2 >= 1);
-console.log(2 < 1);
-console.log(2 == 1);
-console.log(2 != 1);
+console.log(2 > 1); // true
+console.log(2 >= 1); // true
+console.log(2 < 1); // false
+console.log(2 == 1); // false
+console.log(2 != 1); // true
 */
 
 /*
-console.log("2" > 1);
-console.log("02" > 1);
-console.log("2" < 1);
-console.log("02" < 1);
+console.log("2" > 1); // true
+console.log("02" > 1); // true
+console.log("2" < 1); // false
+console.log("02" < 1); // false
 */
 
 /*
