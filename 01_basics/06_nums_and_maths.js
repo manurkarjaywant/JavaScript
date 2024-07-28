f20d15f21d6c13ad09235f23a4c6c549ea38996d
manurkarjaywant <147039486+manurkarjaywant@users.noreply.github.com>
2024-07-28 06:37:55 +0000

    Math methods

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,33 +1,46 @@
+/*
 const score = 400
-//console.log(score);
+console.log(score); // 400
+console.log(typeof score);  // number
+*/
 
+/*
 const balance = new Number(100)
-//console.log(balance);
+console.log(balance);   // [Number: 100]
+console.log(typeof balance);    // object
+console.log(balance.toString().length); // 3
+console.log(balance.toFixed(2));    // 100.00
+*/
 
-//console.log(balance.toString().length);
-//console.log(balance.toFixed(2));
+/*
+const otherNumber = 123.8766
+console.log(otherNumber.toPrecision(2));    // 1.2e+2
+console.log(otherNumber.toPrecision(3));    // 124
+console.log(otherNumber.toPrecision(4));    // 123.9
+*/
 
-const otherNumber = 123.8966
-
-//console.log(otherNumber.toPrecision(3));
-
+/*
 const hundreds = 1000000
-//console.log(hundreds.toLocaleString('en-IN'));
-
-
+console.log(hundreds.toLocaleString());         // 1,000,000 => US number format
+console.log(hundreds.toLocaleString('en-IN'));  // 10,00,000 => Indian number format
+*/
 
 // ************************* Maths ****************************
+/*
+console.log(Math);              //  Object [Math] {}
+console.log(Math.abs(-4));      //  4
+console.log(Math.abs(4));       //  4
+console.log(Math.round(4.6));   //  5
+*/
 
-// console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.abs(4));
-// console.log(Math.round(4.6));
+/*
+console.log(Math.random());                 //  its range is from 0 to 1.
+console.log(Math.random() * 10 );
+console.log(Math.floor(Math.random() * 10 ) + 1);
+*/
 
-// console.log(Math.random());
-// console.log(Math.random() * 10 );
-// console.log(Math.floor(Math.random() * 10 ) + 1);
-
+/*
 const min = 2
 const max = 12
-
 console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+*/
