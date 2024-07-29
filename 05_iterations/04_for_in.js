d668357d764e91baac63ba4b475eec9b33e4bc26
68285_Jaywant <manurkarjaywant@gmail.com>
2024-07-29 15:41:11 +0530

    for in loop

--- 05_iterations/04_for_in.js
+++ 05_iterations/04_for_in.js
@@ -1,28 +1,32 @@
 // ********************* for in *************************************
-
+/*
 const myObject = {
     js: 'javascript',
     cpp: 'C++',
     rb: "ruby",
     swift: "swift by apple"
 }
-
-for (const key in myObject) {
-    //console.log(`${key} shortcut is for ${myObject[key]}`);
+for (const key in myObject) {   // this is how objects can be iterated with the help of for in loop
+    console.log(`${key} shortcut is for ${myObject[key]}`);
 }
-
+*/
+// ----------------------------------------------------------------------------------------------------
+/*
 const programming = ["js", "rb", "py", "java", "cpp"]
 
-for (const key in programming) {
-    //console.log(programming[key]);
+for (const key in programming) {    // arrays can be iterated with the help of for in loop
+    console.log(programming[key]);
 }
+*/
+// ----------------------------------------------------------------------------------------------------
+/*
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
 
-// const map = new Map()
-// map.set('IN', "India")
-// map.set('USA', "United States of America")
-// map.set('Fr', "France")
-// map.set('IN', "India")
-
-// for (const key in map) {
-//     console.log(key);
-// }
+for (const key in map) {    // maps cannot be iterated with the help of for in loop
+    console.log(key);
+}
+*/
