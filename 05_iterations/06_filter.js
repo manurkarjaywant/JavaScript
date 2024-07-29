f01940a3f093d0fb27ad8d5e01488e7e1d9b5373
68285_Jaywant <manurkarjaywant@gmail.com>
2024-07-29 17:10:14 +0530

    filter

--- 05_iterations/06_filter.js
+++ 05_iterations/06_filter.js
@@ -12,7 +12,9 @@
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNums.filter( (num) => {
+// const newNums = myNums.filter( (num) => num > 4 )
+
+// const newNums = myNums.filter( (num) => {  // return keyword is mandatory as we have opened {curly braces}
 //     return num > 4
 // } )
 
