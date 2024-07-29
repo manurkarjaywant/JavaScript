50cb9903e7db316a8db3a5104c044902dd71ddb6
68285_Jaywant <manurkarjaywant@gmail.com>
2024-07-29 15:30:55 +0530

    for of loop

--- 05_iterations/03_for_of.js
+++ 05_iterations/03_for_of.js
@@ -4,39 +4,42 @@
 // ["", "", ""]
 // [{}, {}, {}]
 
+/*
 const arr = [1, 2, 3, 4, 5]
-
 for (const num of arr) {
     //console.log(num);
 }
-
+*/
+// -------------------------------------------------------------
+/*
 const greetings = "Hello world!"
 for (const greet of greetings) {
     //console.log(`Each char is ${greet}`)
 }
-
+*/
+// -------------------------------------------------------------
 // **************** Maps ************************
-
+/*
 const map = new Map()
 map.set('IN', "India")
 map.set('USA', "United States of America")
 map.set('Fr', "France")
-map.set('IN', "India")
+map.set('IN', "India")      // duplicate keys are not allowed // it will not give any error
+map.set('PK', 'India')      // duplicate values are allowed
 
+console.log(map);           // it will give output as => Map(no. of keys){'key1' => 'value1', ......'key_n' => 'value_n'}
 
-// console.log(map);
-
-for (const [key, value] of map) {
-    // console.log(key, ':-', value);
+for (const [key, value] of map) {   // this will give only keys and values as an output
+    console.log(key+ " :- " +value);
 }
-
+*/
+// ----------------------------------------------------------------------------------------------
+/*
 const myObject = {
     game1: 'NFS',
     game2: 'Spiderman'
 }
-
-// for (const [key, value] of myObject) {
-//     console.log(key, ':-', value);
-    
-// }
-
+for (const [key, value] of myObject) {      // it will give error as objects are not iterable with the help of for of loop
+    console.log(key+ " :- " +value);
+}
+*/
