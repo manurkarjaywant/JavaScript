6a0a047cf20a9fcfc9a0d849cb36ef97b8f43108
68285_Jaywant <manurkarjaywant@gmail.com>
2024-07-29 14:58:14 +0530

    while do while

--- 05_iterations/02_while_do-while.js
+++ 05_iterations/02_while_do-while.js
@@ -1,25 +1,30 @@
 // ************* while *********************
-
-// let index = 0
-// while (index <= 10 ) {
-//     console.log(`value of index is ${index}`);
-//     index = index + 2
-// }
-
-// let myArray = ["flash", "superman", "batman"]
-// let index = 0
-// while (index < myArray.length) {
-//     console.log(`value is ${myArray[index]}`);
-//     index = index + 1
-// }
-
+/*
+let index = 0
+while (index <= 10 ) {
+    console.log(`value of index is ${index}`);
+    index = index + 2
+}
+*/
+// -----------------------------------------------------------------------------
+/*
+let myArray = ["flash", "superman", "batman"]
+let index = 0
+while (index < myArray.length) {
+    console.log(`value is ${myArray[index]}`);
+    index = index + 1
+}
+*/
+// -----------------------------------------------------------------------------
 // ************* do while *********************
-
-// let score = 1
-// do {
-//     console.log(`score is ${score}`);
-//     score++
-// } while (score <= 10);
+/*
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
+*/
+// ------------------------------------------------------------------------------
 
 let score = 11
 do {
