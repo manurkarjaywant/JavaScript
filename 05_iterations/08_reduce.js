e8e626de00e7e3d6ce59ebbe19ce3ba2cd9bb1f3
68285_Jaywant <manurkarjaywant@gmail.com>
2024-07-29 17:59:59 +0530

    reduce method

--- 05_iterations/08_reduce.js
+++ 05_iterations/08_reduce.js
@@ -1,5 +1,21 @@
 // ******************************* reduce ************************************
+// *********** syntax **************
+/*
+const array1 = [1, 2, 3, 4];
 
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;     // this value will be stored in accumulator
+const sumWithInitial = array1.reduce( (accumulator, currentValue) => accumulator + currentValue, initialValue,);
+
+// currentValue will be taken from the array on which reduce method is applied
+// initialValue will be stored inside accumulator at the start of iteration
+// after the first iteration the sum of accumulator and currentValue will be stored inside accumulator
+
+
+console.log(sumWithInitial);
+// Expected output: 10
+*/
+// ------------------------------------------------------------------------
 const myNums = [1, 2, 3]
 
 // const myTotal = myNums.reduce(function (acc, currval) {
