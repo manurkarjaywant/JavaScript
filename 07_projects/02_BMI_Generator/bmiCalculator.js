17f38061f26b6a6ccd52f883d27e2f5601ab3154
Jaywant Manurkar <manurkarjaywant@gmail.com>
2024-08-16 18:50:06 +0530

    changes in html

--- 07_projects/02_BMI_Generator/bmiCalculator.js
+++ 07_projects/02_BMI_Generator/bmiCalculator.js
@@ -14,11 +14,11 @@ form.addEventListener('submit',(e)=>{
         const bmi = (weight / ((height * height) / 10000)).toFixed(2)
         //show the result
         if (bmi < 18.6) {
-            results.innerHTML = `<span><h1 style="background-color: red">Your BMI is ${bmi}, You are Under Weight</h1></span>`
+            results.innerHTML = `<span><h1 style="color: red">Your BMI is ${bmi}, You are Under Weight</h1></span>`
         } else if (bmi > 18.6 && bmi < 24.9) {
-            results.innerHTML = `<span><h1 style="background-color:green">Your BMI is ${bmi}, You are Normal Weight</h1></span>`
+            results.innerHTML = `<span><h1 style="color:green">Your BMI is ${bmi}, You are Normal Weight</h1></span>`
         } else {
-            results.innerHTML = `<span><h1 style="background-color:red">Your BMI is ${bmi}, You are Overweight</h1></span>`
+            results.innerHTML = `<span><h1 style="color:red">Your BMI is ${bmi}, You are Overweight</h1></span>`
         }
         
     }
